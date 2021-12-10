745931acc93a8ea34ab681cb57e59ed16db352a2
Jonas Agger <jaj@lindcapital.com>
2021-12-10 16:30:44 +0100

    Added day8

---
+++ Days/Day7.cs
@@ -0,0 +1,74 @@
+﻿using AdventOfCode.Library;
+
+namespace AdventOfCode.Days;
+
+public class Day7 : DayEngine
+{
+    public override string[] TestInput => new string[]
+    {
+        "16,1,2,0,4,2,7,1,2,14"
+    };
+
+    protected override Task<object> HandlePart1(string[] input)
+    {
+        var ints = input[0].Split(',').Select(int.Parse).OrderBy(x => x).ToArray();
+
+        var max = ints.Max();
+
+        var distances = new int[max+1];
+
+        foreach (var i in ints)
+        {
+            for (int j = 0; j < max+1; j++)
+            {
+                distances[j] += Math.Abs(i - j);
+            }
+        }
+
+        var min = -1;
+        var minValue = int.MaxValue;
+
+        for (int i = 0; i < max+1; i++)
+        {
+            if (minValue > distances[i])
+            {
+                minValue = distances[i];
+                min = i;
+            }
+        }
+
+        return Task.FromResult<object>(minValue);
+    }
+
+    protected override Task<object> HandlePart2(string[] input)
+    {
+        var ints = input[0].Split(',').Select(int.Parse).OrderBy(x => x).ToArray();
+
+        var max = ints.Max();
+
+        var distances = new int[max + 1];
+
+        foreach (var i in ints)
+        {
+            for (int j = 0; j < max + 1; j++)
+            {
+                var x = Math.Abs(i - j);
+                distances[j] += x*(x+1)/2;
+            }
+        }
+
+        var min = -1;
+        var minValue = int.MaxValue;
+
+        for (int i = 0; i < max + 1; i++)
+        {
+            if (minValue > distances[i])
+            {
+                minValue = distances[i];
+                min = i;
+            }
+        }
+
+        return Task.FromResult<object>(minValue);
+    }
+}
---
+++ Days/Day8.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode.Library;
+
+namespace AdventOfCode.Days;
+
+public class Day8 : DayEngine
+{
+    public override string[] TestInput => new string[]
+    {
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+    };
+
+    protected override Task<object> HandlePart1(string[] input)
+    {
+        var cnt = 0;
+        foreach (var str in input)
+        {
+            var parts = str.Split('|');
+
+            var identifiers = parts[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => IsLen(x.Length)).Select(x => x.Sum(Convert.ToInt32)).ToArray();
+            var values = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Sum(Convert.ToInt32));
+
+            cnt += values.Count(x => identifiers.Contains(x));
+        }
+
+
+        return Task.FromResult<object>(cnt);
+    }
+
+    protected override Task<object> HandlePart2(string[] input)
+    {
+
+        return Task.FromResult<object>(null);
+    }
+
+    private bool IsLen(int len) => len switch
+    {
+        2 => true,
+        3 => true,
+        4 => true,
+        7 => true,
+        _ => false
+    };
+}
