f16e62a71c822eb50a679f3315fdd12834e0043c
Jonas Agger <jaj@lindcapital.com>
2022-12-14 23:07:01 +0100

    just parsing

--- 2022/Day14.cs
+++ 2022/Day14.cs
@@ -6,18 +6,30 @@ using AdventOfCode.Library;
     {
         public override string[] TestInput => new string[]
         {
-            ""
+            "498,4 -> 498,6 -> 496,6",
+            "503,4 -> 502,4 -> 502,9 -> 494,9"
         };
 
         protected override object HandlePart1(string[] input)
         {
+            var graphs = input.Select(x => x.Split("->")
+                    .Select(y => y.Split(','))
+                    .Select(x => new Point(x[0].Int(), x[1].Int())).ToArray())
+                .ToArray();
+
+            var maxX = graphs.Max(x => x.Max(y => y.X));
+            var maxY = graphs.Max(x => x.Max(y => y.Y));
+
+            var grid = Grid<bool>.Create(maxX, maxY);
+
+            
 
             return null;
         }
 
         protected override object HandlePart2(string[] input)
         {
-
+            
             return null;
         }
     }
